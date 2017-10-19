0fc8e4063df8712eb80fbf464c594a5f412b0cda
BrandonCoats <bgcoats1313@gmail.com>
2017-10-19 08:47:48 -0600

    Made the first use of the lane

--- src/CarWashDriver.java
+++ src/CarWashDriver.java
@@ -5,34 +5,29 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class CarWashDriver extends Thread{
-
-
-
-	
-
 	//Fuck You John; 
 	//Brandon,
 	//Matt,
 	//and Chance
 	//fight me Brandon
 	//int average = (-1) * math.log(1 - gen.nextDouble()) * averageCarWashPerHour;
-
-	
-	public static long dayLength = 720;
+	public static double timePassed = 0;
+	public static int dayLength = 720;
 	public static int maxWashTime = 10;
-	public static long minWashTime = 5;
+	public static int minWashTime = 5;
 	public final int washLinesCount = 1;
-	public int timeTillNewArrivals = 10;
-	public int mostArrivals = 3;
+	public static int timeTillNewArrivals = 10;
+	public static int mostArrivals = 3;
 
 	
-	public static long timePassed = 0;//Use this instead of putting threads to sleep
+	//Use this instead of putting threads to sleep
 	//-Ln(1-.78)(20) =
-	Semaphore washingLines = new Semaphore(washLinesCount);//if we want to have only one person pay at a time
+	static Semaphore washingLines = new Semaphore(washLinesCount);//if we want to have only one person pay at a time
 	
 	
 	static Math math;
 	public static Random gen = new Random();
+	static ExecutorService svc;
 	
 	/*
 	 * I am not sure what kind of Executor service we should use, 
@@ -44,6 +39,18 @@ public class CarWashDriver extends Thread{
 
 		while(timePassed < dayLength)
 		{//while day is not over 
+			double average = ((-1) * math.log(1 - gen.nextDouble()) * timeTillNewArrivals);
+			int peopleArrived = (gen.nextInt(mostArrivals)+1);
+			svc = Executors.newFixedThreadPool(peopleArrived);
+			svc.submit(() ->{
+				try {
+					washingLines.acquire();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					throw new RuntimeException();
+				}
+			});
+			timePassed += average;
 			
 		}
 
